fee0e01cf37b12bd6ba16ee81a75d21f74d5984c
slowly-nake <653362181@qq.com>
2022-02-26 15:05:12 +0800

    Changes

--- my_test.py
+++ my_test.py
@@ -4,3 +4,38 @@
 # a = algorithm.quick_sort(li, orderly=False)
 # print(a)
 
+# class Li():
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#
+#
+# l3 = Li(3)
+# l2 = Li(2, l3)
+# l1 = Li(1, l2)
+#
+# print(l1.val)
+# print(l1.next.val)
+# print(l1.next.next.val)
+
+
+# def cou(a):
+#     n = 0
+#     if a < 1:
+#         return n
+#     elif a < 10:
+#         n += 1
+#         return n
+#     else:
+#         a = a/10
+#         n = cou(a) + 1
+#         return n
+#
+# print(cou(float(input())))
+
+
+# a = [123,345,555,666]
+#
+# print(a.pop())
+#
+# print(a)
