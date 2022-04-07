23856aab2633505c0328c8e4e03ed731a5f6d666
slowly-nake <653362181@qq.com>
2022-04-07 15:07:21 +0800

    三哥帮忙

---
+++ mysql_test.py
@@ -0,0 +1,19 @@
+'''
+@Project: mysql_test.py   #项目名称
+@Description:               #描述
+@Time:2022-04-07 13:46       #日期
+@Author:MING                #创建人
+'''
+
+
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='950102', charset='utf8')
+cursor = conn.cursor()
+cursor.execute('show databases')
+result = cursor.fetchall()
+print(result)
+
+
+cursor.close()
+conn.close()
